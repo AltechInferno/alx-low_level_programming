6f400c5adbb03f994ff855b95c52a9383f61a570
AltechInferno <altechinferno@gmail.com>
2023-02-17 15:18:35 -0800

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-/** 
- * a random number to the variable n each time it is run 
- * it is positive when num is > 0
- * it is negative when num is < 0 
- * return 0
- * 
+/**
+ *    main - random number to the variable n each time it is executed
+ *    Description: if the number is greater than 0: is positive
+ *    if the number is 0: is zero
+ *     if the number is less than 0: is negative
+ * Return: 0
  */
-
 int main(void)
 {
 int n;
