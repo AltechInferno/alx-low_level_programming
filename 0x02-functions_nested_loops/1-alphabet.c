16ecfa335630071d8b41d789520350e37b2bbad3
AltechInferno <altechinferno@gmail.com>
2023-02-21 18:39:33 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet
+ * print all alphabet in lowercase
+ * prints the english alphabet from a-z.
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		/* Description : print_alphabet call putchar from main*/
+		_putchar(letter);
+	}
+	_putchar('\n');
+}
