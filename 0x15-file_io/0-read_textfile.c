bd6d7b55613fa33376c92719b158b20807174081
AltechInferno <altechinferno@gmail.com>
2023-04-10 21:01:21 -0700

    read_text_file_c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,40 +1,36 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * read_textfile - Reads and prints text from a file.
- * @filename: Pointer to the filename string.
- * @letters: Number of letters to read and print.
- *
- * Return: If successful, the actual number of bytes read and printed.
- *         Otherwise, 0.
+ * print_file_contents - reads a text file and prints a specified number of letters
+ * @filename: name of the file to be read
+ * @letters: number of letters to be printed
+ * Return: number of letters printed. Returns 0 on failure.
  */
-ssize_t read_textfile(const char *filename, size_t letters)
+
+ssize_t print_file_contents(const char *filename, size_t letters)
 {
-	ssize_t file_desc, read_bytes, write_bytes;
-	char *buffer;
+    int fd;
+    ssize_t bytes_read, bytes_written;
+    char *buffer;
 
-	if (filename == NULL)
-		return (0);
+    if (!filename)
+        return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-		return (0);
+    fd = open(filename, O_RDONLY);
 
-	file_desc = open(filename, O_RDONLY);
-	read_bytes = read(file_desc, buffer, letters);
-	write_bytes = write(STDOUT_FILENO, buffer, read_bytes);
+    if (fd == -1)
+        return (0);
 
-	if (file_desc == -1 || read_bytes == -1 ||
-		write_bytes == -1 || write_bytes != read_bytes)
-	{
-		free(buffer);
-		return (0);
-	}
+    buffer = malloc(sizeof(char) * (letters));
+    if (!buffer)
+        return (0);
 
-	free(buffer);
-	close(file_desc);
+    bytes_read = read(fd, buffer, letters);
+    bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 
-	return (write_bytes);
+    close(fd);
+
+    free(buffer);
+    return (bytes_written);
 }
 
